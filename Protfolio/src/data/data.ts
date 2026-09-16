b5784728f35167c1168582d638b022401d200fcf
Silvia Sifath <60133577+Silvia-sifath@users.noreply.github.com>
2026-09-17 00:11:54 +0600

    Update data.ts

--- Protfolio/src/data/data.ts
+++ Protfolio/src/data/data.ts
@@ -492,7 +492,7 @@ export const achievementItems: AchievementItem[] = [
   {
     label: 'ICT Fellowship',
     value:
-      'ICT Fellowship for Master’s Thesis — November 2024 to November 2025',
+      'ICT Fellowship for Master’s Thesis — 2024 to 2025',
   },
 
   {
